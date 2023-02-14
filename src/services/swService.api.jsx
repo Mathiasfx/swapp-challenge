05e14229f2256c97158e83fee1aedc7a6d2fa14f
Mathias Pereira <mathias_fx@yahoo.com.ar>
2023-02-14 10:26:38 -0300

    Alert Error agregado por si falla la api

--- src/services/swService.api.jsx
+++ src/services/swService.api.jsx
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 const url = import.meta.env.VITE_APP_API_URL;
-console.log(url);
 
 export const getAll = createAsyncThunk("people/getAll", async () => {
   try {
@@ -9,7 +8,6 @@ export const getAll = createAsyncThunk("people/getAll", async () => {
     return res.data.results;
   } catch (err) {
     alert(err.message);
-
     console.error(err);
     throw Error(err);
   }
